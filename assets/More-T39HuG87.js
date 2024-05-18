49e91917c892baf150da3382eb5a8c915341abb0
AnitaPrzedwojewska <nisiek@wp.pl>
2024-05-18 20:50:50 +0200

    Updates

--- assets/More-T39HuG87.js
+++
@@ -1,1 +0,0 @@
-import{P as s,j as e}from"./index-Bt4Cxn2F.js";const m="_moreElement_bjyi2_1",n="_more_bjyi2_1",o={moreElement:m,more:n},t=({onClick:r})=>e.jsx("div",{className:o.moreElement,children:e.jsx("button",{className:o.more,onClick:r,children:"Load more"})});t.propTypes={onClick:s.func.isRequired};export{t as M};
